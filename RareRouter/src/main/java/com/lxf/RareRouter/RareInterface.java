399c891cdf3cadf6338bc18ca438e13559cbedc2
lvxiaofei <lvxiaofei@pc.linux.com>
2021-11-21 18:53:38 +0800

    code

---
+++ RareRouter/src/main/java/com/lxf/RareRouter/RareInterface.java
@@ -0,0 +1,23 @@
+package com.lxf.RareRouter;
+
+public interface RareInterface {
+    /*
+    method route
+     */
+    <T> T create(final Class<T> service);
+
+    /*
+    如果一个类使用了注解，可以从该方法获取它的class。该方法没有使用反射。
+     */
+    Class<?> annotationClass(String annotationPath);
+
+    /*
+    如果一个类使用了注解，该方法可以获取该类的实例。该方法没有使用反射。
+     */
+    Object annotationClsInstance(String annotationPath);
+
+    /*
+    启动android中的activity。
+     */
+    void startActivity(Object context, String activityAnnotation);
+}
--- RareRouter/src/main/java/com/lxf/RareRouter/RareRouter.java
+++ RareRouter/src/main/java/com/lxf/RareRouter/RareRouter.java
@@ -1,39 +1,20 @@
 package com.lxf.RareRouter;
 
-import com.lxf.init.RouteBean;
-import com.lxf.init.RouteMap;
-
-import java.lang.reflect.Proxy;
-
 public class RareRouter {
 
     public static <T> T create(final Class<T> service) {
-        return (T) Proxy.newProxyInstance(service.getClassLoader(),
-                new Class<?>[]{service},
-                new RouterHandler(service));
+        return RareInterfaceImp.getInstance().create(service);
     }
 
-    public static Class<?> annotationClass(String activityAnnotationPath) {
-        RouteBean routeBean = RouteMap.getInstance().getClassRouteBean(activityAnnotationPath);
-        if (routeBean == null) {
-            return null;
-        }
-        return RouteMap.getInstance().getClazz(routeBean.pkgName);
+    public static Class<?> annotationClass(String annotationPath) {
+        return RareInterfaceImp.getInstance().annotationClass(annotationPath);
     }
 
     public static Object annotationClsInstance(String annotationPath) {
-        RouteBean routeBean = RouteMap.getInstance().getClassRouteBean(annotationPath);
-        if (routeBean == null) {
-            return null;
-        }
-        return RouteMap.getInstance().getObject(routeBean.pkgName);
+        return RareInterfaceImp.getInstance().annotationClsInstance(annotationPath);
     }
 
-    public static void startActivity(Object context, String activityAnnotationPath) {
-        Class<?> cls = annotationClass(activityAnnotationPath);
-        if (cls == null) {
-            return;
-        }
-        RouteMap.getInstance().startActivity(context, cls);
+    public static void startActivity(Object context, String activityAnnotation) {
+        RareInterfaceImp.getInstance().startActivity(context, activityAnnotation);
     }
 }
