fdefbd22f112e5bec3cea2c941a4321ee7df2a55
lvxiaofei <lvxiaofei@win10>
2022-02-06 11:21:09 +0800

    code

--- RareRouter/src/main/java/com/lxf/Process/configure/RareXml.java
+++ RareRouter/src/main/java/com/lxf/Process/configure/RareXml.java
@@ -1,6 +1,8 @@
 package com.lxf.Process.configure;
 
 public class RareXml {
+    public static final String RouterName = "RareRouter";
+
     public static boolean isXmlExists = false;
     public static String appModule = null;
     public static String logDir = null;
--- RareRouter/src/main/java/com/lxf/Process/configure/TxtPath.java
+++ RareRouter/src/main/java/com/lxf/Process/configure/TxtPath.java
@@ -1,13 +1,12 @@
 package com.lxf.Process.configure;
 
-import com.lxf.Application;
 import com.lxf.Process.base.BaseProcessor;
 import com.lxf.Process.genTxt.TxtCreator;
 
 
 public class TxtPath {
 
-    private final String BUILD_DIR = Application.RouterName;
+    private final String BUILD_DIR = RareXml.RouterName;
     private final String BUILD_DIR_PROCESS = "process";
 
     private final String ROUTE_SCAN_RES = "scan.txt";
@@ -20,24 +19,15 @@ public class TxtPath {
 
 
     public void init() {
-        initPath();
-        createTxtIfNotExist();
-    }
-
-    private void initPath() {
-
         String base_dir = RareXml.logDir + BaseProcessor.systemDirPathSplit + BUILD_DIR +
                 BaseProcessor.systemDirPathSplit + BUILD_DIR_PROCESS + BaseProcessor.systemDirPathSplit;
         PATH_SCAN_RES = base_dir + BaseProcessor.moduleName + "_" + ROUTE_SCAN_RES;
         LOG_INFO = RareXml.logDir + BaseProcessor.systemDirPathSplit + BUILD_DIR +
                 BaseProcessor.systemDirPathSplit + LOG_TXT;
         Log_Dir = base_dir;
-    }
 
-
-    private void createTxtIfNotExist() {
-        /*
-          以此创建1个目录、4个文件；
+         /*
+          以此创建1个目录、2个文件；
         */
         TxtCreator.createDir(Log_Dir);
         TxtCreator.createFileIfNone(PATH_SCAN_RES);
